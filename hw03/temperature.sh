1505ee2711201f85b8d0b12768ab2bddcc20e89b
hangdu <2580392278@qq.com>
2016-09-19 14:19:01 -0400

    modify temperature.sh

--- hw03/temperature.sh
+++ hw03/temperature.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-temp=`i2cget -y 2 0x4a 0`
+temp1=`i2cget -y 2 0x4a 0`
 
-temp2=$(($temp))
+temp1_1=$(($temp1))
 
-echo "$temp2*1.8+32" | bc
+temp2=`i2cget -y 2 0x49 0`
+
+temp2_1=$(($temp2))
+
+echo "temperature of 0x4a is"
+echo "$temp1_1*1.8+32" | bc
+
+echo "temperature of 0x49 is"
+echo "$temp2_1*1.8+32" | bc
 
